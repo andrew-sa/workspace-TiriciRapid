3744a02c9866e9326b3b69cc80d3159028cdd93d
Giovanni <Giovanni@172.19.1.193>
2018-01-24 16:30:28 +0100

    Lista professori

--- TirociRapid/src/it/tirocirapid/tirocinio/richiesta/CaricaListaProfessori.java
+++ TirociRapid/src/it/tirocirapid/tirocinio/richiesta/CaricaListaProfessori.java
@@ -1,11 +1,22 @@
 package it.tirocirapid.tirocinio.richiesta;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import it.tirocirapid.classes.manager.AbstractAziendaManager;
+import it.tirocirapid.classes.manager.AbstractProfessoreManager;
+import it.tirocirapid.classes.model.Azienda;
+import it.tirocirapid.classes.model.Professore;
+import it.tirocirapid.factory.AbstractManagerFactory;
+import it.tirocirapid.factory.DAOFactory;
 
 /**
  * Servlet che gestisce il caricamento della lista dei professori che uno studente può scegliere come tutor interno per la propria richiesta di tirocinio
@@ -26,8 +37,21 @@ public class CaricaListaProfessori extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		AbstractManagerFactory factory = new DAOFactory();
+		AbstractProfessoreManager managerProfessore = factory.createProfessoreManager();
+		try 
+		{
+			ArrayList<Professore> professori = managerProfessore.readAll();
+			request.setAttribute("professori", professori);
+			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/"); //Alla schermata della ListaProfessori
+			dispatcher.forward(request, response);	
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+			String referer = request.getHeader("Referer");
+			response.sendRedirect(referer);
+		}
 	}
 
 	/**
