9b8ec75404a3c90598aab8b2f703c2877bca529a
Giovanni <Giovanni@172.19.1.193>
2018-01-24 16:17:52 +0100

    Lista Aziende

--- TirociRapid/src/it/tirocirapid/tirocinio/richiesta/CaricaListaAziende.java
+++ TirociRapid/src/it/tirocirapid/tirocinio/richiesta/CaricaListaAziende.java
@@ -1,11 +1,20 @@
 package it.tirocirapid.tirocinio.richiesta;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import it.tirocirapid.classes.manager.AbstractAziendaManager;
+import it.tirocirapid.classes.model.Azienda;
+import it.tirocirapid.factory.AbstractManagerFactory;
+import it.tirocirapid.factory.DAOFactory;
 
 /**
  * Servlet che si occupa di gestire il carimento delle aziende
@@ -26,8 +35,22 @@ public class CaricaListaAziende extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-	{
-		
+	{	
+		AbstractManagerFactory factory = new DAOFactory();
+		AbstractAziendaManager managerAzienda = factory.createAziendaManager();
+		try 
+		{
+			ArrayList<Azienda> aziende = managerAzienda.readAll();
+			request.setAttribute("aziende", aziende);
+			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/"); //Alla schermata della ListaAziende
+			dispatcher.forward(request, response);	
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+			String referer = request.getHeader("Referer");
+			response.sendRedirect(referer);
+		}
 	}
 
 	/**
