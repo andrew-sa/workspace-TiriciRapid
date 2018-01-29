ba367f91d2ec525a0f9dfe4de6231d72b1bb373c
antoninotomeo <a.tomeo5@studenti.unisa.it>
2018-01-29 09:57:23 +0100

    Prova

--- TirociRapid/src/it/tirocirapid/autenticazione/CreaCurriculumStudente.java
+++ TirociRapid/src/it/tirocirapid/autenticazione/CreaCurriculumStudente.java
@@ -13,14 +13,14 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/inserisci_curriculum")
 public class CreaCurriculumStudente extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CreaCurriculumStudente() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public CreaCurriculumStudente() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -34,11 +34,11 @@ public class CreaCurriculumStudente extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		
-		
+
+
+
 	}
-	
+
 	/**
 	 * 
 	 * @param fax Il fax da validare
@@ -71,5 +71,5 @@ public class CreaCurriculumStudente extends HttpServlet {
 	{
 		return false;
 	}
-	
+
 }
