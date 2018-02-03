c76c775bcdf5a70c221092e5bdbc71bbe0cf1e66
antoninotomeo <a.tomeo5@studenti.unisa.it>
2018-02-03 16:43:31 +0100

    Completato lo scheletro delle Classi di test DAO

---
+++ TirociRapid/src/it/tirocirapid/test/unit/TestProfessoreDAO.java
@@ -0,0 +1,23 @@
+package it.tirocirapid.test.unit;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import junit.framework.TestCase;
+
+public class TestProfessoreDAO extends TestCase{
+
+	@BeforeEach
+	protected void setUp(){
+	}
+
+	@AfterEach
+	protected void tearDown(){
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
