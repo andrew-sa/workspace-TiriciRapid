f235ea35f034a2a837fda3f853c586052e821363
antoninotomeo <a.tomeo5@studenti.unisa.it>
2018-02-04 18:18:00 +0100

    Aggiunti i primi metodi per il test di AziendaDAO risolto problema di "aziendadaosearch query" e private static is nelle classi AziendaDAO e RichiestaTirocioDAO

--- TirociRapid/src/it/tirocirapid/test/unit/TestAziendaDAO.java
+++ TirociRapid/src/it/tirocirapid/test/unit/TestAziendaDAO.java
@@ -1,5 +1,7 @@
 package it.tirocirapid.test.unit;
-
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.sql.Connection;
@@ -7,6 +9,9 @@ import java.sql.SQLException;
 import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+
+import com.mysql.jdbc.ResultSet;
+import com.mysql.jdbc.Statement;
 
 import it.tirocirapid.classes.manager.AziendaDAO;
 import it.tirocirapid.classes.model.Azienda;
@@ -16,54 +21,125 @@ import it.tirocirapid.eccezioni.TuplaNotFoundException;
 
 public class TestAziendaDAO {
 	
-	protected AziendaDAO aziendaDAO;
-	protected AziendaDAO aziendaDAO2;
-	protected Azienda azienda;
-	protected Connection con;
 	
 	@BeforeEach
 	public void setUp() throws Exception {
-		aziendaDAO=new AziendaDAO();
-		aziendaDAO2=new AziendaDAO();
-		azienda=new Azienda("partitaIVA","password","email","nome","sede","3333333333",null,"stato","descrizioneAmbito");
-		con=DriverManagerConnectionPool.getIstance().getConnection();
+
+		
 	}
 
 	@AfterEach
 	public void tearDown() throws Exception {
-		azienda=null;
-		aziendaDAO=null;
-		aziendaDAO2=null;
-		con.close();
+		
 	}
 	
 	@Test
 	public void testSearch() throws SQLException, TuplaNotFoundException {
-		//aziendaDAO2.search("07643520567", "ciao");
+		AziendaDAO aziendaDAOsearch= new AziendaDAO();
 		
-	}
+		try(Connection connection=DriverManagerConnectionPool.getIstance().getConnection();)
+        {
+            try(Statement stCheck=(Statement) connection.createStatement())
+            {
+                connection.setAutoCommit(false);
+                
+                /*Essendo un metodo che restituisce boolean posso fare il confronto con due booleani*/
+                Boolean controllo;
+                Boolean bool=true;
+                String iva="07643520567";
+                String pass="ciao";
+                controllo=aziendaDAOsearch.search(iva,pass);
+                assertEquals(bool,controllo);
+            }
+            finally
+            {
+                
+                 connection.close();;
+            }
+        }	
+    }
 
 	@Test
 	public void testCreate() throws SQLException, InsertFailedException {
-		fail("Not yet implemented");
-		//aziendaDAO.create(azienda);
-		/*Verifico nella base di fati se l'ho salvato*/
-		/*
-		PreparedStatement ps = con.prepareStatement("SELECT PartitaIVA FROM azienda WHERE PartitaIVA = ?");
-		ResultSet rs = ps.executeQuery();
-		String aaa=rs.getString("PartitaIVA");
-        assertEquals("partitaIVA",aaa);*/
-	    	
+		AziendaDAO aziendaDAOcreate= new AziendaDAO();
+		Azienda azienda;
+		try(Connection connection=DriverManagerConnectionPool.getIstance().getConnection();)
+        {
+            try(Statement stCheck=(Statement) connection.createStatement())
+            {
+                connection.setAutoCommit(false);
+                /*CANCELLARE LA TUPLA DI TESTIG PRIMA DI ESEGUIRE*/
+                //stCheck.executeUpdate("DELETE FROM azienda WHERE PartitaIVA='partitaIVA'");
+
+                String partitaIVA="partitaIVA";
+                String password="password";
+                String email="email";
+                String nome="nome";
+                String sede="sede";
+                String numero="3333333333";
+                String stato="stato";
+                String descrizioneAmbito="descrizioneAmbito";
+                
+                azienda=new Azienda(partitaIVA,password,email,nome,sede,numero,null,stato,descrizioneAmbito);
+                aziendaDAOcreate.create(azienda);
+
+                String partitaIVAID;
+                try(ResultSet rs=(ResultSet) stCheck.executeQuery("SELECT * FROM azienda WHERE PartitaIVA='partitaIVA'"))
+                {
+                	
+                    assertTrue(rs.next());
+                    partitaIVAID=rs.getString("PartitaIVA");
+                    System.out.println("SONO NEL TEST CREATE:"+partitaIVAID);
+                	
+                }   
+            }
+                
+                 connection.close();
+        }
+	}
+	
+	@Test  /*PROBLEMA DI VISIBILITA'  E' UN METODO STATICO PRIVATO*/
+	public void testIsNewKey() throws SQLException, TuplaNotFoundException {
+		fail("Not yet implemented");	
+		/*	
+				try(Connection connection=DriverManagerConnectionPool.getIstance().getConnection();)
+		        {
+		            try(Statement stCheck=(Statement) connection.createStatement())
+		            {
+		           
+		            	Boolean falso= false;
+		            	Boolean vero=true;
+		            	String iva="07643520567";
+		                String pass="ciao";
+		            	
+		              
+		            
+		            }
+		            finally
+		            {
+		                
+		                 connection.close();;
+		            }
+		        }	
+		*/
 	}
 	
 	@Test
-	public void testIsNewKey() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
-	public void testRead() {
-		fail("Not yet implemented");
+	public void testRead() throws SQLException, TuplaNotFoundException {
+		AziendaDAO aziendaDAOread= new AziendaDAO();
+		Azienda az;
+				try(Connection connection=DriverManagerConnectionPool.getIstance().getConnection();)
+		        {
+		            try(Statement stCheck=(Statement) connection.createStatement())
+		            {
+		                connection.setAutoCommit(false);
+		                String iva="07643520567";
+		                az=aziendaDAOread.read(iva);
+		                assertEquals(iva,az.getPartitaIVA());
+		                System.out.println("SONO NEL TEST READ:"+iva);
+		            }
+		            connection.close();
+		        }
 	}
 	
 	
